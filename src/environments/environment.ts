92e78734ec66a628c4e8585b837810fb87cdf345
Erik Galiana Farell <erikgalianafarell@MacBook-Pro-de-Erik.local>
2020-05-08 15:50:49 +0200

    connect to firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCsXtI8l_Ng8GEaPErz-VDTfynBEeml9XY',
+    authDomain: 'typescript-platzi-f4976.firebaseapp.com',
+    databaseURL: 'https://typescript-platzi-f4976.firebaseio.com',
+    projectId: 'typescript-platzi-f4976',
+    storageBucket: 'typescript-platzi-f4976.appspot.com',
+    messagingSenderId: '774405944476',
+    appId: '1:774405944476:web:c9882fa8e2646b856ee959',
+  },
 };
 
 /*
